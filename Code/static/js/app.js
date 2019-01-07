484077aa5e5516203c86ba9788381c1d0cfa9983
John Doe <johndoe@users.noreply.github.com>
2019-01-07 13:11:24 -0500

    Axander- index.html and app.js files updated. Filtering works but not as dropdown menu.

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,62 +1,92 @@
-// from data.js
+// Get references to the tbody element and datatable
 var tableData = data;
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
 var table = d3.select("table");
-//table.attr("class", "table table-striped");
 
-// Console.log the UFO data from the data.js
-console.log(data);
+//var cityInput = d3.select("#city").property("value");
+//var countryInput = d3.select("#country").property("value");
+// Select the submit filter button
+var searchFilter = d3.select("#filter-btn");
+var resetTable = d3.select("#reset-btn");
+/*********************************************
+ * Render table
+*/
+function createTable(tableContent)
+{
+    var tbody = d3.select("tbody");
+   d3.select("tbody").html("");
+   for (var i=0; i < tableContent.length; i++){
+       tr = tbody.append("tr");
+       tr.append("td").text(tableContent[i].datetime);
+       tr.append("td").text(tableContent[i].city);
+       tr.append("td").text(tableContent[i].state.toUpperCase());
+       tr.append("td").text(tableContent[i].country.toUpperCase());
+       tr.append("td").text(tableContent[i].shape);
+       tr.append("td").text(tableContent[i].durationMinutes);
+       tr.append("td").text(tableContent[i].comments);
+   }; // end for statement
 
-// Refractor to use Arrow functions
-data.forEach((ufoReport) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(([key,value]) => {
-        var cell = tbody.append("td");
-        cell.text(value);
-    });
+}; // end CreateTable
 
-});
+// Display unfiltered table when pg loads for first time
+createTable(tableData);
+/*******************************************
+ * Start event handler function
+ * ****************************************
+*/
+// Add event listener for searchFilter, call the SearchButtonClick function
+searchFilter.on("click", handleSearchButtonClick);
+resetTable.on("click", handleResetButtonClick);
 
-// Filter code
-
-var reports = data;
-
-// Select the submit button
-
-var submit = d3.select("#filter-btn");
-
-// Listner event
-submit.on("click", function(){
-    
+// define the handleSearchButtonClick function
+function handleSearchButtonClick()
+{
     //prevent the page from refreshing
     d3.event.preventDefault();
+    var filterData = tableData;
+    // filter dataTime input
+    var dateInput = d3.select("#datetime").property("value");
+    var cityInput =  d3.select("#city").property("value");
+    var stateInput =  d3.select("#state").property("value");
+    var countryInput =  d3.select("#country").property("value");
+    var shapeInput =  d3.select("#shape").property("value")
+    
+    if (dateInput){
+        filterData = tableData.filter(data => data.datetime === dateInput);
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    };
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    if (cityInput){
+        filterData = tableData.filter(data => data.city === cityInput);
 
-  console.log(inputValue);
-  console.log(reports);
+    };
 
-  // Filter data
+    if (stateInput){
+        filterData = tableData.filter(data => data.state === countryInput.toString().toLowerCase());
 
-  var filterData = reports.filter(report => report.datetime === inputValue);
+    };
+    
 
-  console.log(filterData);
+    if (countryInput){
+        filterData = tableData.filter(data => data.country === countryInput.toString().toLowerCase());
 
-  // var filterValue = filterData.property("value");
+    };
 
-  console.log(filterData);
+    if (shapeInput){
+        filterData = tableData.filter(data => data.shape === shapeInput.toString().toLowerCase());
 
- // d3.select("table").text(filterData);
+    };
+    
+    
+    
+    // generate table with filtered data
+    createTable(filterData);
+}; // end handleSearchButtonClick
+
+// define the handleResetButtonClick function
+function handleResetButtonClick(){
+    createTable(tableData);
+
+}
 
 
-
-
-});
-
-// YOUR CODE HERE!
